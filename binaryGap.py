decf652c20e86615a8a1140059f6d9405c509608
dbores3 <101841633+dbores3@users.noreply.github.com>
2022-05-30 12:29:51 -0500

    Update binaryGap.py

--- binaryGap.py
+++ binaryGap.py
@@ -1,4 +1,5 @@
-/*A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
+"""
+A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
 
 For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps. The number 32 has binary representation 100000 and has no binary gaps.
 
@@ -15,7 +16,8 @@ Write an efficient algorithm for the following assumptions:
 N is an integer within the range [1..2,147,483,647].*/
 
 # you can write to stdout for debugging purposes, e.g.
-# print("this is a debug message")
+# print("this is a debug message")"""
+
 
 def solution(N):
     # write your code in Python 3.6
