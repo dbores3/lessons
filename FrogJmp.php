9f295422451651bca4c308105ffe860cefec5b5a
dbores3 <101841633+dbores3@users.noreply.github.com>
2022-06-04 13:19:05 -0500

    Update FrogJmp.php

--- FrogJmp.php
+++ FrogJmp.php
@@ -23,5 +23,6 @@ Write an efficient algorithm for the following assumptions:
 X, Y and D are integers within the range [1..1,000,000,000];
 X ≤ Y.*/
 function solution($X, $Y, $D) {
+    //Converts the value into integer
     return intval(floor((($Y-$X) - 1 + $D) / $D)); //range is Y-X, the furthest is (Y-X) -1  & the next hop is (Y-X) - + D & rounds to the nearest integer with floor
 }
