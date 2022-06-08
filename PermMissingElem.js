4e18b9f131c9f0e6d8f6a98725e857e5250ba8c2
dbores3 <101841633+dbores3@users.noreply.github.com>
2022-06-08 11:08:25 -0500

    Update PermMissingElem.js

--- PermMissingElem.js
+++ PermMissingElem.js
@@ -21,3 +21,19 @@ Write an efficient algorithm for the following assumptions:
 N is an integer within the range [0..100,000];
 the elements of A are all distinct;
 each element of array A is an integer within the range [1..(N + 1)].*/
+function solution(A) {
+    //Gets the length of the Array plus 1
+    const N = A.length + 1
+    //Checks if the array is empty
+    if(A.length > 0)
+      //Sums all the values in the array
+      sum = A.reduce((accumulator, current) => accumulator + current) //the reducer walks through the array in each element, adding the previous one to the new calculation
+    else
+      //if empty, sets 0
+      sum = 0
+
+    //Time complexity formula
+    result = Math.floor(N * (N+1) / 2) - sum
+    
+    return result
+}
